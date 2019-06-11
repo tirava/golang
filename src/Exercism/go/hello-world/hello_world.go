9ee928e64be80af4f57e3187b8266bb4156c0ffa
Eugene Klimov <tirava@gmail.com>
2019-06-11 22:53:14 +0300

    Hello complete

--- src/Exercism/go/hello-world/hello_world.go
+++ src/Exercism/go/hello-world/hello_world.go
@@ -11,5 +11,5 @@ func HelloWorld() string {
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	return ""
+	return "Hello, World!"
 }
