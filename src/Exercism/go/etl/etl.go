b127a0cc4e6fa973f5bf805bab6cb36f8dedd309
Eugene Klimov <tirava@gmail.com>
2019-06-21 11:52:37 +0300

    Maps in range iteration = key, value!

--- src/Exercism/go/etl/etl.go
+++ src/Exercism/go/etl/etl.go
@@ -3,20 +3,30 @@ package etl
 
 import "strings"
 
-type Give map[int][]string
-type Expect map[string]int
+//type Give map[int][]string
+//type Expect map[string]int
+//
+//// Transform returns transformated score table.
+//func Transform(in Give) Expect {
+//	out := Expect{}
+//	for i := 1; i <= 10; i++ {
+//		if _, ok := in[i]; !ok {
+//			continue
+//		}
+//		for _, letter := range in[i] {
+//			out[strings.ToLower(letter)] = i
+//		}
+//	}
+//
+//	return out
+//}
 
-// Transform returns transformated score table.
-func Transform(in Give) Expect {
-	out := Expect{}
-	for i := 1; i <= 10; i++ {
-		if _, ok := in[i]; !ok {
-			continue
-		}
-		for _, letter := range in[i] {
-			out[strings.ToLower(letter)] = i
+func Transform(input map[int][]string) map[string]int {
+	result := map[string]int{}
+	for value, list := range input {
+		for _, c := range list {
+			result[strings.ToLower(c)] = value
 		}
 	}
-
-	return out
+	return result
 }
