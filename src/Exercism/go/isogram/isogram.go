de371ca5b70b66e74087d13a1ed32f3e9bda7983
Eugene Klimov <tirava@gmail.com>
2019-06-25 13:22:52 +0300

    test rune

--- src/Exercism/go/isogram/isogram.go
+++ src/Exercism/go/isogram/isogram.go
@@ -3,22 +3,45 @@ package isogram
 
 import (
 	"strings"
+	"unicode"
 )
 
 // IsIsogram returns true if string is isogram.
 func IsIsogram(phrase string) bool {
 
-	phrase = strings.ToLower(phrase)
-
-	for _, s := range phrase{
-		s := string(s)
-		if s == " " || s == "-" {
+	//phrase = strings.ToLower(phrase)
+	phraseRune := []rune(phrase)
+	//count := 0
+	for i, r := range phraseRune {
+		if !unicode.IsLower(r) {
+			phraseRune[i] = unicode.ToLower(r)
+		}
+	}
+	//unicode.strings.ToLower()
+	s := ""
+	for _, r := range phraseRune {
+		//su := unicode.ToLower(s)
+		//s := string(su)
+		if r == ' ' || r == '-' { // need unicode.IsLetter
 			continue
 		}
-		if strings.Count(phrase, s) > 1 {
+		s = string(r) + s
+		if strings.Count(phrase, r) > 1 {
 			return false
 		}
 	}
 
 	return true
 }
+
+//func countRune(rs []rune, rc rune) (count int) {
+//	for _, r := range rs {
+//		if r == rc {
+//			count++
+//			if count > 1 {
+//				return
+//			}
+//		}
+//	}
+//	return
+//}
