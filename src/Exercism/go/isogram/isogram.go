47fa27a480143cd802a740089554ee5855df0f57
Eugene Klimov <tirava@gmail.com>
2019-06-26 21:48:30 +0300

    Best performance isogram

--- src/Exercism/go/isogram/isogram.go
+++ src/Exercism/go/isogram/isogram.go
@@ -1,30 +1,26 @@
 // Package isogram implements determination if a word or phrase is an isogram.
 package isogram
 
-import (
-	"unicode"
+import "strings"
+
+const (
+	space  = ' '
+	hyphen = '-'
 )
 
 // IsIsogram returns true if string is isogram.
 func IsIsogram(phrase string) bool {
 
-	phraseRune := []rune(phrase)
+	phrase = strings.ToLower(phrase)
 
-	for i, r := range phraseRune {
-		phraseRune[i] = unicode.ToLower(r)
-	}
-
-	for _, s := range phraseRune {
-		if !unicode.IsLetter(s) {
+	for i, s := range phrase {
+		if s == space || s == hyphen {
 			continue
 		}
-		count := 0
-		for _, c := range phraseRune {
-			if s == c {
-				count++
-				if count > 1 {
-					return false
-				}
+
+		for j := i + 1; j < len(phrase); j++ {
+			if phrase[j] == byte(s) {
+				return false
 			}
 		}
 	}
