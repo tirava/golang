b8a912086db1ee4e2675b548174b6a2b6aa3c10e
Eugene Klimov <tirava@gmail.com>
2019-07-03 20:11:02 +0300

    Rune solution - bad

--- src/Exercism/go/isogram/isogram.go
+++ src/Exercism/go/isogram/isogram.go
@@ -1,30 +1,48 @@
 // Package isogram implements determination if a word or phrase is an isogram.
 package isogram
 
-import "strings"
-
-const (
-	space  = ' '
-	hyphen = '-'
+import (
+	"strings"
+	"unicode"
 )
 
+//const (
+//	space  = ' '
+//	hyphen = '-'
+//)
+
 // IsIsogram returns true if string is isogram.
-func IsIsogram(phrase string) bool {
+//func IsIsogram(phrase string) bool {
+//
+//	phrase = strings.ToLower(phrase)
+//
+//	for i, s := range phrase {
+//		if s == space || s == hyphen {
+//			continue
+//		}
+//
+//		for j := i + 1; j < len(phrase); j++ {
+//			//if phrase[j] == byte(s) {
+//			if rune(phrase[j]) == s {
+//				return false
+//			}
+//		}
+//	}
+//
+//	return true
+//}
+func IsIsogram(input string) bool {
+	var foundRunes = map[rune]bool{}
+	input = strings.ToLower(input)
 
-	phrase = strings.ToLower(phrase)
-
-	for i, s := range phrase {
-		if s == space || s == hyphen {
+	for _, val := range input {
+		if !unicode.IsLetter(val) {
 			continue
 		}
-
-		for j := i + 1; j < len(phrase); j++ {
-			//if phrase[j] == byte(s) {
-			if rune(phrase[j]) == s {
-				return false
-			}
+		if foundRunes[val] {
+			return false
 		}
+		foundRunes[val] = true
 	}
-
 	return true
 }
