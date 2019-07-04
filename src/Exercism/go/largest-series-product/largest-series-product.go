a2d582a234e1739a11c46daa1840d36eb49215a3
Eugene Klimov <tirava@gmail.com>
2019-07-04 22:26:45 +0300

    Largest Series Product - fix

--- src/Exercism/go/largest-series-product/largest-series-product.go
+++ src/Exercism/go/largest-series-product/largest-series-product.go
@@ -9,13 +9,17 @@ func LargestSeriesProduct(digits string, span int) (product int64, err error) {
 	var curProd, maxProd int64
 	curProd = 1
 
+	if span > len(digits) || span < 0 {
+		return -1, errors.New("span must be greater than zero or span must be smaller than string length")
+	}
+
 	for i := 0; i <= len(digits)-span; i++ {
 		curProd = 1
 		for j := i; j < i+span; j++ {
 			//println("i, j", i, j)
 			n := digits[j] - 48
 			if n < 0 || n > 9 {
-				return -1, errors.New("span must be greater than zero")
+				return -1, errors.New("digits input must only contain digits")
 			}
 			curProd *= int64(n)
 			//println("curProd=", curProd)
