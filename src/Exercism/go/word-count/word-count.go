56b96ecbef677fd13e4dc0d9da6a7b8cebfab1e5
Eugene Klimov <tirava@gmail.com>
2019-07-06 12:00:38 +0300

    Word Count best sols

--- src/Exercism/go/word-count/word-count.go
+++ src/Exercism/go/word-count/word-count.go
@@ -9,22 +9,57 @@ import (
 type Frequency map[string]int
 
 // WordCount counts every word in phrase.
-func WordCount(input string) (counts Frequency) {
+//func WordCount(input string) (counts Frequency) {
+//
+//	counts = make(Frequency)
+//
+//	input = strings.ToLower(input)
+//	reg, _ := regexp.Compile("[^a-z0-9']+")
+//	input = reg.ReplaceAllString(input, "_")
+//
+//	words := strings.Split(input, "_")
+//
+//	for _, word := range words {
+//		if word == "" {
+//			continue
+//		}
+//		word = strings.Trim(word, "'")
+//		counts[word]++
+//	}
+//	return counts
+//}
 
-	counts = make(Frequency)
+//func WordCount(phrase string) Frequency {
+//
+//	phrase = strings.ToLower(phrase)
+//
+//	var result = make(Frequency)
+//
+//	f := func(c rune) bool {
+//		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && c != '\''
+//	}
+//
+//	words := strings.FieldsFunc(phrase, f)
+//
+//	for _, word := range words {
+//		word = strings.Trim(word, "'")
+//		result[word]++
+//	}
+//
+//	return result
+//}
 
-	input = strings.ToLower(input)
-	reg, _ := regexp.Compile("[^a-z0-9']+")
-	input = reg.ReplaceAllString(input, "_")
+//type Frequency map[string]int
 
-	words := strings.Split(input, "_")
+var reg = regexp.MustCompile("[a-z0-9]+('[a-z0-9])*")
 
-	for _, word := range words {
-		if word == "" {
-			continue
-		}
-		word = strings.Trim(word, "'")
-		counts[word]++
+// WordCount return the frequencies of a word in a string
+func WordCount(s string) Frequency {
+	s = strings.ToLower(s)
+	result := Frequency{}
+	words := reg.FindAllString(s, -1)
+	for _, c := range words {
+		result[c]++
 	}
-	return counts
+	return result
 }
