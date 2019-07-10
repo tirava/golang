873574acd829db171fbc965a51edacd51d6d5d36
Eugene Klimov <tirava@gmail.com>
2019-07-10 23:58:18 +0300

    Cryptosquare - middle

--- src/Exercism/go/crypto-square/crypto-square.go
+++ src/Exercism/go/crypto-square/crypto-square.go
@@ -2,6 +2,7 @@
 package cryptosquare
 
 import (
+	"math"
 	"regexp"
 	"strings"
 )
@@ -12,6 +13,14 @@ var r = regexp.MustCompile("[^0-9a-zA-Z]+")
 func Encode(pt string) (ct string) {
 	ct = r.ReplaceAllString(pt, "")
 	ct = strings.ToLower(ct)
-	//println(ct)
+	q := int(math.Sqrt(float64(len(ct))))
+	c := q + 1
+	r := len(ct) / q
+	ll := c - c*raaaaaaaaaaaaaaaaaa
+	sl := make([]string, 0)
+	for i := 0; i < q; i++ {
+		//sl = append(sl, ct[i*(q+1) : 2*i*(q+1)])
+	}
+	println(c, r, ll, sl)
 	return
 }
