14ee15bc8613b1f0b3630c96c4fdb9e6bc11075d
Eugene Klimov <tirava@gmail.com>
2019-07-14 21:32:56 +0300

    Pythagorean-Triplet fix bench

--- src/Exercism/go/pythagorean-triplet/pythagorean-triplet.go
+++ src/Exercism/go/pythagorean-triplet/pythagorean-triplet.go
@@ -6,20 +6,11 @@ type Triplet [3]int
 // Sum returns a list of all Pythagorean triplets where the sum a+b+c
 // (the perimeter) is equal to p.
 func Sum(sum int) (out []Triplet) {
-	min, max := 1, sum
-	out0 := append(out, Triplet{0, 0, 0})
-	for a := min; a <= max; a++ {
-		for b := a; b <= max; b++ {
-			for c := b; c <= max; c++ {
-				if c*c == a*a+b*b {
-					if a+b+c == sum {
-						out0 = append(out0, Triplet{a, b, c})
-					}
-				}
-			}
+	for _, x := range Range(1, sum/2) {
+		if x[0]+x[1]+x[2] == sum {
+			out = append(out, x)
 		}
 	}
-	out = out0[1:]
 	return
 }
 
